ed4d6ef58a379832e34d712f58c5cbe47a9a29f4
CAFÉ <pciskolargx@gmail.com>
2025-11-04 14:11:05 -0300

    commita e commita

--- src/rotas/appsRotas.js
+++ src/rotas/appsRotas.js
@@ -1,22 +1,21 @@
 import React from 'react';
-import { StatusBar, SafeAreaView } from 'react-native';
-import useProdutores from './src/hooks/useProdutores';
 import {NavigationContainer} from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { createStackNavigator } from '@react-navigation/stack';
 import Home from './src/telas/Home';
 import melhoresProdutores from './src/telas/Produtores/MelhoresProdutores';
-import attRotas from './src/rotas/appsRotas';
-import appRotas from './src/rotas/appsRotas';
 
 const tab = createBottomTabNavigator();
 const stack = createStackNavigator();
 
+export default function appRotas(){
+    return  <NavigationContainer>
+      <tab.Navigator>
+        <tab.Screen name="Home" component={Home} />
+        <tab.Screen name="Produtores" component={melhoresProdutores} />
+        
+      </tab.Navigator>
+      <melhoresProdutores/>
 
-export default function App() {
-
-  return <SafeAreaView style={{ flex: 1 }}>
-    <StatusBar />
-    <appRotas/>
-  </SafeAreaView>
+    </NavigationContainer>
 }

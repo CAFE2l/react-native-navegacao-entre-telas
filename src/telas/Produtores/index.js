0a0a7e1f951725ba5ecc06e8e7ff454abc6c8442
CAFÉ <pciskolargx@gmail.com>
2025-11-07 17:43:53 -0300

    estilos e remoção da mensagem

--- src/telas/Produtores/index.js
+++ src/telas/Produtores/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { FlatList, Text, StyleSheet } from 'react-native';
 import { useNavigation, useRoute } from '@react-navigation/native';
 import Produtor from './componentes/Produtor';
@@ -9,15 +9,29 @@ import useTextos from '../../hooks/useTextos';
 export default function Produtores({ melhoresProdutores }) {
   const navigation = useNavigation();
   const route = useRoute();
+  const [ exibeMensagem,  setExibeMensagem ] = useState(false);
   const nomeCompra = route.params?.compra.nome;
-  console.log(nomeCompra);
+
+  const timestampCompra = route.params?.compra.timestamp;
+  console.log(timestampCompra);
+  
   const lista = useProdutores(melhoresProdutores);
-  const { tituloProdutores } = useTextos();
+  const { tituloProdutores, mensagemCompra } = useTextos();
+  const mesagemCompleta = mensagemCompra?.repalce('$NOME', nomeCompra);
+
+  useEffect(() => {
+    setExibeMensagem(!!nomeCompra);
+    const timeout = setTimeout(() => {
+      setExibeMensagem(false);
+    }, 3000);
+
+    return () => clearTimeout(timeout);
+  }, [timestampCompra]);
   
   const TopoLista = () => {
     return <>
       <Topo melhoresProdutores={melhoresProdutores} />
-      <Text>{ nomeCompra }</Text>
+      { exibeMensagem && <Text style={estilos.compra}>{ mensagemCompleta }</Text>}
       <Text style={estilos.titulo}>{tituloProdutores}</Text>
     </>
   }
@@ -45,5 +59,14 @@ const estilos = StyleSheet.create({
     marginTop: 16,
     fontWeight: 'bold',
     color: '#464646',
+  },
+  compra: {
+    backgroundColor: '#EAF5F3',
+    padding: 16,
+    color: '#464646',
+    fontSize: 16,
+    lineHeight: 26,
+    fontWeight: 'bold',
+    textAlign: 'center',
   }
 })

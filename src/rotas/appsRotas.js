bdf9eb494e8c06b7815f7586b6d8807f4edc8698
CAFÉ <pciskolargx@gmail.com>
2025-11-07 16:26:08 -0300

    Adição dos ícones

--- src/rotas/appsRotas.js
+++ src/rotas/appsRotas.js
@@ -6,13 +6,29 @@ import Home from './src/telas/Home';
 import melhoresProdutores from './src/telas/Produtores/MelhoresProdutores';
 import ProdutorRotas from './produtorRotas';
 import melhoresProdutoresRotas from './melhoresProdutoresRotas';
+import Coracao from '../assets/coracao.svg';
+import Home from '../assets/home.svg';
 
 const tab = createBottomTabNavigator();
 const stack = createStackNavigator();
 
 export default function appRotas(){
     return  <NavigationContainer>
-      <tab.Navigator screenOptions={{ headerShown: false }}>
+      <tab.Navigator screenOptions={ ({ route }) => ({
+        headerShown: false, 
+        tabBarIcon: ({ color }) => {
+          let Icon = Home;
+
+          if (route.name === 'Melhores Produtores'){
+            Icon = Coracao;
+          }
+
+          return <Icon color={color}/>
+        },
+        tabBarActiveTintColor:  '#2A9F85', 
+        tabBarInactiveTintColor: '#C7C7C7', 
+
+        })}>
         <tab.Screen name="Home" component={ProdutorRotas} />
         <tab.Screen name="Produtores" component={melhoresProdutoresRotas} />
         

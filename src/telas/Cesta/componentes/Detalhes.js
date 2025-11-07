8b49a208a14702943941274a83accdf5e7efa092
CAFÉ <pciskolargx@gmail.com>
2025-11-07 17:02:34 -0300

    simulação de compra

--- src/telas/Cesta/componentes/Detalhes.js
+++ src/telas/Cesta/componentes/Detalhes.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { Image, StyleSheet, View, TouchableOpacity } from 'react-native';
-
+import { useNavigation } from '@react-navigation/native';
 import useTextos from '../../../hooks/useTextos';
 import Texto from '../../../componentes/Texto';
 
 export default function Detalhes({ nome, produtor, descricao, preco }) {
+ const navigation = useNavigation();
   const { botaoComprar } = useTextos();
 
   return <>
@@ -18,7 +19,7 @@ export default function Detalhes({ nome, produtor, descricao, preco }) {
 
     <TouchableOpacity 
       style={estilos.botao} 
-      onPress={() => {}}>
+      onPress={() => navigation.popToTop()}>
       <Texto style={estilos.textoBotao}>{ botaoComprar }</Texto>
     </TouchableOpacity>
   </>

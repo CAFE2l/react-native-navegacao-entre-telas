37d4a6247bd1b1ae1ae4dc139242c2a42b1c6b5e
CAFÉ <pciskolargx@gmail.com>
2025-11-07 15:15:43 -0300

    envio de dados ao produtor

--- src/telas/Produtor/index.js
+++ src/telas/Produtor/index.js
@@ -1,7 +1,12 @@
 import React from 'react';
 import { Text } from 'react-native';
-
+import { useRoute } from '@react-navigation/native';
 
 export default function Produtor() {
+    const route = useRoute();
+    const produtor = route.params;
+    console.log(produtor);
+
+
     return <Text>Produtor</Text>
 }
